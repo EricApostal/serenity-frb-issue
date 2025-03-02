daf72e128e19af2ac9700ccc1467fc9cf0c1cdee
EricApostal <60072374+EricApostal@users.noreply.github.com>
2025-03-02 14:18:21 -0500

    manual compile FRB works, but many errors exist

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:serenity_dart_compat/src/rust/api/simple.dart';
 import 'package:serenity_dart_compat/src/rust/frb_generated.dart';
+import 'package:serenity_dart_compat/src/rust/lib.dart';
+import 'package:serenity_dart_compat/src/rust/third_party/serenity/client.dart';
 
 Future<void> main() async {
   await RustLib.init();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    print(ClientBuilder);
+  }
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/src/rust/third_party/serenity/model/application.freezed.dart
@@ -0,0 +1,309 @@
+// dart format width=80
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'application.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+// dart format off
+T _$identity<T>(T value) => value;
+/// @nodoc
+mixin _$MessageInteractionMetadata {
+
+ RustOpaqueInterface get field0;
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is MessageInteractionMetadata&&(identical(other.field0, field0) || other.field0 == field0));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,field0);
+
+@override
+String toString() {
+  return 'MessageInteractionMetadata(field0: $field0)';
+}
+
+
+}
+
+/// @nodoc
+class $MessageInteractionMetadataCopyWith<$Res>  {
+$MessageInteractionMetadataCopyWith(MessageInteractionMetadata _, $Res Function(MessageInteractionMetadata) __);
+}
+
+
+/// @nodoc
+
+
+class MessageInteractionMetadata_Command extends MessageInteractionMetadata {
+  const MessageInteractionMetadata_Command(this.field0): super._();
+  
+
+@override final  MessageCommandInteractionMetadata field0;
+
+/// Create a copy of MessageInteractionMetadata
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$MessageInteractionMetadata_CommandCopyWith<MessageInteractionMetadata_Command> get copyWith => _$MessageInteractionMetadata_CommandCopyWithImpl<MessageInteractionMetadata_Command>(this, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is MessageInteractionMetadata_Command&&(identical(other.field0, field0) || other.field0 == field0));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,field0);
+
+@override
+String toString() {
+  return 'MessageInteractionMetadata.command(field0: $field0)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class $MessageInteractionMetadata_CommandCopyWith<$Res> implements $MessageInteractionMetadataCopyWith<$Res> {
+  factory $MessageInteractionMetadata_CommandCopyWith(MessageInteractionMetadata_Command value, $Res Function(MessageInteractionMetadata_Command) _then) = _$MessageInteractionMetadata_CommandCopyWithImpl;
+@useResult
+$Res call({
+ MessageCommandInteractionMetadata field0
+});
+
+
+
+
+}
+/// @nodoc
+class _$MessageInteractionMetadata_CommandCopyWithImpl<$Res>
+    implements $MessageInteractionMetadata_CommandCopyWith<$Res> {
+  _$MessageInteractionMetadata_CommandCopyWithImpl(this._self, this._then);
+
+  final MessageInteractionMetadata_Command _self;
+  final $Res Function(MessageInteractionMetadata_Command) _then;
+
+/// Create a copy of MessageInteractionMetadata
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') $Res call({Object? field0 = null,}) {
+  return _then(MessageInteractionMetadata_Command(
+null == field0 ? _self.field0 : field0 // ignore: cast_nullable_to_non_nullable
+as MessageCommandInteractionMetadata,
+  ));
+}
+
+
+}
+
+/// @nodoc
+
+
+class MessageInteractionMetadata_Component extends MessageInteractionMetadata {
+  const MessageInteractionMetadata_Component(this.field0): super._();
+  
+
+@override final  MessageComponentInteractionMetadata field0;
+
+/// Create a copy of MessageInteractionMetadata
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$MessageInteractionMetadata_ComponentCopyWith<MessageInteractionMetadata_Component> get copyWith => _$MessageInteractionMetadata_ComponentCopyWithImpl<MessageInteractionMetadata_Component>(this, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is MessageInteractionMetadata_Component&&(identical(other.field0, field0) || other.field0 == field0));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,field0);
+
+@override
+String toString() {
+  return 'MessageInteractionMetadata.component(field0: $field0)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class $MessageInteractionMetadata_ComponentCopyWith<$Res> implements $MessageInteractionMetadataCopyWith<$Res> {
+  factory $MessageInteractionMetadata_ComponentCopyWith(MessageInteractionMetadata_Component value, $Res Function(MessageInteractionMetadata_Component) _then) = _$MessageInteractionMetadata_ComponentCopyWithImpl;
+@useResult
+$Res call({
+ MessageComponentInteractionMetadata field0
+});
+
+
+
+
+}
+/// @nodoc
+class _$MessageInteractionMetadata_ComponentCopyWithImpl<$Res>
+    implements $MessageInteractionMetadata_ComponentCopyWith<$Res> {
+  _$MessageInteractionMetadata_ComponentCopyWithImpl(this._self, this._then);
+
+  final MessageInteractionMetadata_Component _self;
+  final $Res Function(MessageInteractionMetadata_Component) _then;
+
+/// Create a copy of MessageInteractionMetadata
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') $Res call({Object? field0 = null,}) {
+  return _then(MessageInteractionMetadata_Component(
+null == field0 ? _self.field0 : field0 // ignore: cast_nullable_to_non_nullable
+as MessageComponentInteractionMetadata,
+  ));
+}
+
+
+}
+
+/// @nodoc
+
+
+class MessageInteractionMetadata_ModalSubmit extends MessageInteractionMetadata {
+  const MessageInteractionMetadata_ModalSubmit(this.field0): super._();
+  
+
+@override final  MessageModalSubmitInteractionMetadata field0;
+
+/// Create a copy of MessageInteractionMetadata
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$MessageInteractionMetadata_ModalSubmitCopyWith<MessageInteractionMetadata_ModalSubmit> get copyWith => _$MessageInteractionMetadata_ModalSubmitCopyWithImpl<MessageInteractionMetadata_ModalSubmit>(this, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is MessageInteractionMetadata_ModalSubmit&&(identical(other.field0, field0) || other.field0 == field0));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,field0);
+
+@override
+String toString() {
+  return 'MessageInteractionMetadata.modalSubmit(field0: $field0)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class $MessageInteractionMetadata_ModalSubmitCopyWith<$Res> implements $MessageInteractionMetadataCopyWith<$Res> {
+  factory $MessageInteractionMetadata_ModalSubmitCopyWith(MessageInteractionMetadata_ModalSubmit value, $Res Function(MessageInteractionMetadata_ModalSubmit) _then) = _$MessageInteractionMetadata_ModalSubmitCopyWithImpl;
+@useResult
+$Res call({
+ MessageModalSubmitInteractionMetadata field0
+});
+
+
+
+
+}
+/// @nodoc
+class _$MessageInteractionMetadata_ModalSubmitCopyWithImpl<$Res>
+    implements $MessageInteractionMetadata_ModalSubmitCopyWith<$Res> {
+  _$MessageInteractionMetadata_ModalSubmitCopyWithImpl(this._self, this._then);
+
+  final MessageInteractionMetadata_ModalSubmit _self;
+  final $Res Function(MessageInteractionMetadata_ModalSubmit) _then;
+
+/// Create a copy of MessageInteractionMetadata
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') $Res call({Object? field0 = null,}) {
+  return _then(MessageInteractionMetadata_ModalSubmit(
+null == field0 ? _self.field0 : field0 // ignore: cast_nullable_to_non_nullable
+as MessageModalSubmitInteractionMetadata,
+  ));
+}
+
+
+}
+
+/// @nodoc
+
+
+class MessageInteractionMetadata_Unknown extends MessageInteractionMetadata {
+  const MessageInteractionMetadata_Unknown(this.field0): super._();
+  
+
+@override final  InteractionType field0;
+
+/// Create a copy of MessageInteractionMetadata
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$MessageInteractionMetadata_UnknownCopyWith<MessageInteractionMetadata_Unknown> get copyWith => _$MessageInteractionMetadata_UnknownCopyWithImpl<MessageInteractionMetadata_Unknown>(this, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is MessageInteractionMetadata_Unknown&&(identical(other.field0, field0) || other.field0 == field0));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,field0);
+
+@override
+String toString() {
+  return 'MessageInteractionMetadata.unknown(field0: $field0)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class $MessageInteractionMetadata_UnknownCopyWith<$Res> implements $MessageInteractionMetadataCopyWith<$Res> {
+  factory $MessageInteractionMetadata_UnknownCopyWith(MessageInteractionMetadata_Unknown value, $Res Function(MessageInteractionMetadata_Unknown) _then) = _$MessageInteractionMetadata_UnknownCopyWithImpl;
+@useResult
+$Res call({
+ InteractionType field0
+});
+
+
+
+
+}
+/// @nodoc
+class _$MessageInteractionMetadata_UnknownCopyWithImpl<$Res>
+    implements $MessageInteractionMetadata_UnknownCopyWith<$Res> {
+  _$MessageInteractionMetadata_UnknownCopyWithImpl(this._self, this._then);
+
+  final MessageInteractionMetadata_Unknown _self;
+  final $Res Function(MessageInteractionMetadata_Unknown) _then;
+
+/// Create a copy of MessageInteractionMetadata
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') $Res call({Object? field0 = null,}) {
+  return _then(MessageInteractionMetadata_Unknown(
+null == field0 ? _self.field0 : field0 // ignore: cast_nullable_to_non_nullable
+as InteractionType,
+  ));
+}
+
+
+}
+
+// dart format on
